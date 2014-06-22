3de30800bb31499faeea634737e6e8154a5dc8e4
Wang Chen <wangvsa@163.com>
2014-06-22 14:18:25 +0800

    加入获取access_token功能

--- functions.php
+++ functions.php
@@ -47,5 +47,67 @@ function wechat_get_thumb( $post, $size ){
     return $thumb;
 }
 
+/**
+ * GET 请求
+ * @param string $url
+ * @return string $result
+ */
 
+function http_get($url) {
+  $oCurl = curl_init();
+  if(stripos($url,"https://")!==FALSE) {
+    curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+  }
+
+  curl_setopt($oCurl, CURLOPT_URL, $url);
+  curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+
+  $result = curl_exec($oCurl);
+  $status = curl_getinfo($oCurl);
+  curl_close($oCurl);
+
+  if(intval($status["http_code"])==200)
+    return $result;
+  return $status["http_code"];
+}
+
+
+/**
+ * POST 请求
+ * @param string $url
+ * @param array $param
+ * @return string $result
+ */
+function http_post($url, $param) {
+
+  $oCurl = curl_init();
+
+  if(stripos($url,"https://")!==FALSE){
+    curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+	}
+
+	if (is_string($param)) {
+	   $strPOST = $param;
+	} else {
+    $aPOST = array();
+    foreach($param as $key=>$val) {
+      $aPOST[] = $key."=".urlencode($val);
+		}
+		$strPOST =  join("&", $aPOST);
+  }
+
+  curl_setopt($oCurl, CURLOPT_URL, $url);
+  curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+  curl_setopt($oCurl, CURLOPT_POST,true);
+  curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
+
+	$result = curl_exec($oCurl);
+	$status = curl_getinfo($oCurl);
+	curl_close($oCurl);
+	if(intval($status["http_code"])==200)
+    return $result;
+  return $status["http_code"];
+}
 ?>
